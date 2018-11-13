584043a5933ce94ed76d99adb888963d038ba64b
Devaraja H E <devandroid2006@gmail.com>
2018-11-13 15:37:56 +0530

    initial commit

---
+++ app/src/main/java/basemodule/devandroid/com/basemodule/BaseActivity.kt
@@ -0,0 +1,12 @@
+package basemodule.devandroid.com.basemodule
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+    }
+}
